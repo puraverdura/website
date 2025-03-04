58f30ac6c09c76e8514f56a73073acd30ed15d08
Leo Roettele <leo.roettele@liip.ch>
2025-03-04 18:33:40 +0100

    add 404 page

---
+++ src/routes/site-not-found/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import PageTeasers from "$lib/components/PageTeasers.svelte";
+</script>
+
+<div class="container">
+	<div
+		class="text-center flex flex-col justify-center gap-y-[6px] md:gap-y-[12px]"
+	>
+		<h1>Diese Seite wurde nicht gefunden</h1>
+		<div>
+			<a
+				href="/"
+				class="bg-primary-100 !text-white !font-heading !font-bold rounded-[4px] !p-[10px] !text-[20px] !mt-[20px] inline-block grow-on-hover hover:bg-primary-120"
+				>Zur Startseite</a
+			>
+		</div>
+	</div>
+</div>
